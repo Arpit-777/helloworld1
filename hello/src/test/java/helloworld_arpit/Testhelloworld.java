d2fac46fdf26600bd9dc5f3438d8d312e5d74291
Arpit Ahuja <aahuja_me20@thapar.edu>
2021-05-07 01:38:18 +0530

    first commit

---
+++ hello/src/test/java/helloworld_arpit/Testhelloworld.java
@@ -0,0 +1,11 @@
+package helloworld_arpit;
+import org.testng.annotations.Test;
+
+public class Testhelloworld {
+	
+	@Test
+	public void testhelloworld() {
+		
+		System.out.println(">>>>>Hello Gurpal Sir!<<<<<<");	}
+
+}
